c492068fee979023adb2ac2edf97b49286e45c3e
YuppyItuEnak <fmuhammad05@Student.ciputra.ac.id>
2022-10-30 12:39:06 +0700

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,25 @@
+@include('navbar')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HomePage</title>
+</head>
+
+<body>
+
+    <div class="">
+        <h1 class="text-8xl text-center dark:text-gray-900">RENEKOPI</h1>
+        <h1 class="text-xl text-md text-center dark:text-gray-900 ">To Fullfil Your Coffee Passion</h1>
+    </div>
+
+
+    
+</body>
+
+</html>
+@include('footer')
