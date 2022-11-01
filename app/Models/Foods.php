70e250f28c0665bfa4fb7fc134a5d078acc5cafb
Reefuu <rifqiet@gmail.com>
2022-11-01 23:31:07 +0700

    kemonpeo

--- app/Models/Foods.php
+++ app/Models/Foods.php
@@ -11,7 +11,95 @@ class Food extends Model
 
     private static $foods = [
         [
-            
-        ]
+            'name' => 'Nasi Ayam Teriyaki',
+            'price' => '20.000',
+            'desc' => 'hand-trimmed chicken breast, served with our teriyaki sauce',
+            'code' =>'nat'
+        ],
+        [
+            'name' => 'Nasi Ayam Lada Hitam',
+            'price' => '20.000',
+            'desc' => 'hand-trimmed chicken breast, served with our blackpepper sauce',
+            'code' =>'nalh'
+        ],
+        [
+            'name' => 'Nasi Ayam Asam Manis',
+            'price' => '20.000',
+            'desc' => 'hand-trimmed chicken breast, served with our sweet sour sauce',
+            'code' =>'naam'
+        ],
+        [
+            'name' => 'Nasi Ayam BBQ',
+            'price' => '20.000',
+            'desc' => 'hand-trimmed chicken breast, served with our barbeque sauce',
+            'code' =>'nabbq'
+        ],
+        [
+            'name' => 'Nasi Ayam Katsu Sambal Matah',
+            'price' => '25.000',
+            'desc' => 'seasoned breaded chicken breast and served with rice',
+            'code' =>'naksm'
+        ],
+        [
+            'name' => 'Pasta Carbonara',
+            'price' => '25.000',
+            'desc' => 'creamy sauce pasta with smoked beef and mushroom',
+            'code' =>'pc'
+        ],
+        [
+            'name' => 'Pasta Aglio e Olio',
+            'price' => '25.000',
+            'desc' => 'only garlic flavour with chili flakes, smoked beef and mushroom',
+            'code' =>'pao'
+        ],
+        [
+            'name' => 'Nasi Goreng Rene',
+            'price' => '23.000',
+            'desc' => 'mix vegetables, minced chicken ad fried egg',
+            'code' =>'ngr'
+        ],
+        [
+            'name' => 'Kentang Goreng',
+            'price' => '16.000',
+            'desc' => 'fried fries',
+            'code' =>'kg'
+        ],
+        [
+            'name' => 'Roti Bakar Strawberry',
+            'price' => '16.000',
+            'desc' => 'strawberry toast',
+            'code' =>'rbs'
+        ],
+        [
+            'name' => 'Roti Bakar Coklat',
+            'price' => '16.000',
+            'desc' => 'chocolate toast',
+            'code' =>'rbc'
+        ],
+        [
+            'name' => 'Roti Bakar Tiramisu',
+            'price' => '16.000',
+            'desc' => 'tiramisu toast',
+            'code' =>'rbt'
+        ],
+        [
+            'name' => 'Tahu Cabe Garam',
+            'price' => '16.000',
+            'desc' => 'fried tofu with salt and chili',
+            'code' =>'tcg'
+        ],
+        [
+            'name' => 'Tahu Barbeque',
+            'price' => '16.000',
+            'desc' => 'fried tofu with barbeque sauce',
+            'code' =>'tbbq'
+        ],
+        [
+            'name' => 'Tahu Crispy',
+            'price' => '14.000',
+            'desc' => 'fried tofu',
+            'code' =>'tc'
+        ],
+
     ];
 }
--- app/Models/NonCoffees.php
+++ app/Models/NonCoffees.php
@@ -8,4 +8,138 @@ use Illuminate\Database\Eloquent\Model;
 class NonCoffee extends Model
 {
     use HasFactory;
+
+    private static $noncoffees = [
+        [
+            'name' => 'Matcha',
+            'price' => '20.000',
+            'code' =>'mtc'
+        ],
+        [
+            'name' => 'Taro',
+            'price' => '18.000',
+            'code' =>'tro'
+        ],
+        [
+            'name' => 'Red Velvet',
+            'price' => '18.000',
+            'code' =>'rvt'
+        ],
+        [
+            'name' => 'Choco Banana',
+            'price' => '18.000',
+            'code' =>'cbnn'
+        ],
+        [
+            'name' => 'Milkshake Chocolate',
+            'price' => '18.000',
+            'code' =>'mchc'
+        ],
+        [
+            'name' => 'Milkshake Vanilla',
+            'price' => '18.000',
+            'code' =>'mvnl'
+        ],
+        [
+            'name' => 'Pina Colada',
+            'price' => '18.000',
+            'code' =>'pico'
+        ],
+        [
+            'name' => 'Milk Tea',
+            'price' => '16.000',
+            'code' =>'mtea'
+        ],
+        [
+            'name' => 'Rhum Raisin',
+            'price' => '18.000',
+            'code' =>'rhra'
+        ],
+        [
+            'name' => 'Earl Grey Milk Tea',
+            'price' => '18.000',
+            'code' =>'egmt'
+        ],
+        [
+            'name' => 'Grey Raisin Tea',
+            'price' => '16.000',
+            'code' =>'grt'
+        ],
+        [
+            'name' => 'Exotic Strawberry River',
+            'price' => '16.000',
+            'code' =>'esr'
+        ],
+        [
+            'name' => 'Summer Tea',
+            'price' => '16.000',
+            'code' =>'stea'
+        ],
+        [
+            'name' => 'Strawberry Squash',
+            'price' => '16.000',
+            'code' =>'ssqh'
+        ],
+        [
+            'name' => 'Lychee Squash',
+            'price' => '16.000',
+            'code' =>'lsqh'
+        ],
+        [
+            'name' => 'Summer Tea',
+            'price' => '16.000',
+            'code' =>'stea'
+        ],
+        [
+            'name' => 'Orange Squash',
+            'price' => '16.000',
+            'code' =>'osqh'
+        ],
+        [
+            'name' => 'Strawberry Tea',
+            'price' => '16.000',
+            'code' =>'sbtea'
+        ],
+        [
+            'name' => 'Lychee Tea',
+            'price' => '16.000',
+            'code' =>'ltea'
+        ],
+        [
+            'name' => 'Lemon Tea',
+            'price' => '16.000',
+            'code' =>'lmte'
+        ],
+        [
+            'name' => 'Original Tea',
+            'price' => '12.000',
+            'code' =>'orte'
+        ],
+        [
+            'name' => 'Ice Cream',
+            'price' => '15.000',
+            'code' =>'icream'
+        ],
+        [
+            'name' => 'Fresh Line Water',
+            'price' => '12.000',
+            'code' =>'flw'
+        ],
+        [
+            'name' => 'Hot Ginger',
+            'price' => '12.000',
+            'code' =>'hgng'
+        ],
+        [
+            'name' => 'Fresh Milk',
+            'price' => '12.000',
+            'code' =>'fmlk'
+        ],
+        [
+            'name' => 'Mineral Water',
+            'price' => '6.000',
+            'code' =>'mw'
+        ],
+
+    ];
 }
