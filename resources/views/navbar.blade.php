b9bac9b6161dd11d7fdd8e16f0c06c12ffaee5b0
YuppyItuEnak <fmuhammad05@Student.ciputra.ac.id>
2022-11-01 23:30:45 +0700

    pokemon

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="http://fonts.cdnfonts.com/css/basier-square-narrow" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/my-coffee-break" rel="stylesheet">
+    
+    <link rel="stylesheet" href={{ asset('/js/page.js') }}>
     <link rel="stylesheet" href={{ asset('/css/app.css') }}>
     <title>{{ $title }}</title>
 
@@ -15,13 +18,18 @@
 </head>
 
 <body>
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
-
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script> --}}
 
     <nav class="navbar sticky-top  navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-lg-5" >
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          
+          <a class="navbar-brand" href="#">
+            <img src="Images/Renekopi3.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+            ReneKopi</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -31,7 +39,7 @@
                 <a class="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/product">Product</a>
+                <a class="nav-link active" aria-current="page" href="#product">Product</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#about">About</a>
@@ -40,6 +48,7 @@
           </div>
         </div>
       </nav>
+      <script type="text/javascript" src={{ asset('/js/page.js') }}></script>
 
 {{-- 
 </body>
