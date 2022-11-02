258d55f8a5f30f495248b31ad44a47ee9779a548
reefu <rifqiet@gmail.com>
2022-11-02 18:49:48 +0700

    FIXXXXXXX

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,23 @@
 <nav class="navbar sticky-top  navbar-expand-lg bg-dark navbar-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img src={{ asset('Images/Renekopi3.png') }} alt="Logo" width="30" height="24"
-                class="d-inline-block align-text-top">
-            ReneKopi</a>
+        @if (Request::url() === 'http://renekopilibrary.test/' ||
+            Request::url() === 'http://renekopilibrary.test/#' ||
+            Request::url() === 'http://renekopilibrary.test' ||
+            Request::url() === 'http://renekopilibrary.test/#product' ||
+            Request::url() === 'http://renekopilibrary.test/#about' ||
+            Request::url() === 'http://127.0.0.1:8000' ||
+            Request::url() === 'http://127.0.0.1:8000/' ||
+            Request::url() === 'http://127.0.0.1:8000/#' ||
+            Request::url() === 'http://127.0.0.1:8000/#product' ||
+            Request::url() === 'http://127.0.0.1:8000/#about')
+            <a class="navbar-brand" href="#">
+            @else
+                <a class="navbar-brand" href="/">
+        @endif
+        <img src={{ asset('Images/Renekopi3.png') }} alt="Logo" width="30" height="24"
+            class="d-inline-block align-text-top">
+        ReneKopi</a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -13,12 +27,23 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#product">Product</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#about">About</a>
-                </li>
+                @if (Request::url() === 'http://renekopilibrary.test/' ||
+                    Request::url() === 'http://renekopilibrary.test/#' ||
+                    Request::url() === 'http://renekopilibrary.test' ||
+                    Request::url() === 'http://renekopilibrary.test/#product' ||
+                    Request::url() === 'http://renekopilibrary.test/#about' ||
+                    Request::url() === 'http://127.0.0.1:8000' ||
+                    Request::url() === 'http://127.0.0.1:8000/' ||
+                    Request::url() === 'http://127.0.0.1:8000/#' ||
+                    Request::url() === 'http://127.0.0.1:8000/#product' ||
+                    Request::url() === 'http://127.0.0.1:8000/#about')
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#product">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#about">About</a>
+                    </li>
+                @endif
             </ul>
         </div>
     </div>
