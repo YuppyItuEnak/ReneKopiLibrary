ff9c56f58545c54bf67351b4c45b64cd70202cd3
Reefuu <rifqiet@gmail.com>
2022-11-02 10:31:12 +0700

    sdawdwsxcbrw

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-black w-100 h-25 p-5">
+<div class="bg-dark w-100 h-25 p-5">
     <footer>
         <p class="text-center text-muted">© Renekopi</p>
     </footer>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.app')
 
 @section('body')
-<h1 class="text-center ">{{ $title }}</h1>
-<div class="container text-center ">
-    <div class="row ">
-        @foreach ($coffees as $coffee)
-            <div class="col-auto col-md-4 mx-auto ">
-                <div class="card my-4 mx-auto" style="width: 15rem; background-color:#dce6ff">
-                    <img src="{{ $coffee['image'] }}" class="card-img-top" style=" height: 230px" alt="Product picture">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $coffee['name'] }}</h5>
-                        <div class="d-flex">
-                            <a href="/detailproduct/{{ $coffee['code'] }}" style="background-color:#516ab0; color: #ffffff"  class="btn  align-items-center justify-content-center mx-auto">Details</a>
+<div style="background: #3f4e4f">
+    <h1 class="text-center" style="color: #dcd7c9">{{ $title }}</h1>
+    <div class="container text-center ">
+        <div class="row ">
+            @foreach ($coffees as $coffee)
+                <div class="col-auto col-md-4 mx-auto ">
+                    <div class="card my-4 mx-auto" style="height: 23rem; width: 16rem; background-color:#dcd7c9;">
+                        <img src="{{ $coffee['image'] }}" class="card-img-top" style=" height: 230px" alt="Product picture">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $coffee['name'] }}</h5>
+                            <div class="d-flex">
+                                <a href="/coffees/{{ $coffee['code'] }}" style="background-color:#516ab0; color: #ffffff"  class="btn  align-items-center justify-content-center mx-auto">Details</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 </div>
+
 @endsection
